da648f55cdcb9ef35d42ab1d66dfbce3fc99c2da
triluong21 <triluong21@gmail.com>
2019-05-16 11:35:24 -0500

    Add joi to validate user's input

--- controllers/users.js
+++ controllers/users.js
@@ -1,8 +1,10 @@
 module.exports = {
   signUp: async (req, res, next) => {
+    // Email and Password
     console.log("UsersController.signUp() called.")
   },
   signIn: async (req, res, next) => {
+    // Generate Token
     console.log("UsersController.signIn() called.")
   },
   secret: async (req, res, next) => {

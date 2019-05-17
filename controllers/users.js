3a392c09d56aac4951b0747ea4691d9c6b9b92d9
triluong21 <triluong21@gmail.com>
2019-05-17 08:55:24 -0500

    Generate/Assign Json Web Token (JWT)

--- controllers/users.js
+++ controllers/users.js
@@ -1,4 +1,15 @@
+const JWT = require('jsonwebtoken');
 const User = require('../models/user');
+const { JWT_SECRET } = require('../configuration/index');
+
+signToken = user => {
+  return JWT.sign({
+    iss: 'Tri Luong',
+    sub: 'user.id',
+    iat: new Date().getTime(),
+    exp: new Date().setDate(new Date().getDate() + 1) // current time plus 1 day ahead
+  }, JWT_SECRET);
+}
 
 module.exports = {
   signUp: async (req, res, next) => {
@@ -6,15 +17,16 @@ module.exports = {
     // check if there is a user with the same email
     const foundUser = await User.findOne({ email });
     if (foundUser) {
-      return res.status(403).send({ error: 'Email is already in use'})
+      return res.status(403).send({ error: 'Email is already in use' })
     }
 
     // Create a new user
     const newUser = new User({ email, password });
     await newUser.save();
 
-    // Response with token
-    res.json({ user: 'created' });
+    // Generate token
+    const token = signToken(newUser);
+    res.status(200).json({ token });
   },
   signIn: async (req, res, next) => {
     // Generate Token
